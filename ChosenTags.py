d1c01e6f73282d090dc82b633764a5062ca07146
Taylor Keppler <tkeppler@macalester.edu>
2015-12-08 21:43:53 -0600

    Switch to .py files, working trimDB file

---
+++ ChosenTags.py
@@ -0,0 +1,68 @@
+
+# coding: utf-8
+
+# In[1]:
+
+LOVE = ['Love', 'romantic', 'love songs', 'love song', 'lovesongs', 'romance', 'lovesong', 'amor']
+
+PRETTY = ['beautiful', 'pretty']
+
+RELAXING = ['chillout', 'chill', 'chill out', 'chilled', 'chill-out', 'chillax', 'relaxed', 'relaxing', 'peaceful', 'Relaxed', 'calm', 'calming']
+
+SAD = ['sad', 'sad song', 'sadness', 'melancholy', 'melancholic', 'depressing']
+
+HAPPY = ['happy', 'feel good', 'feelgood', 'makes me happy', 'good mood', 'cheerful', 'feel good music', 'joyful', 'uplifting']
+
+DREAMY = ['dreamy', 'dream']
+
+NOSTALGIC = ['nostalgia', 'nostalgic', 'sentimental']
+
+MOODY = ['emotional', 'moody', 'emotive', 'mood music']
+
+ANGRY = ['angry']
+
+BREAKUP = ['breakup']
+
+MIS = ['mellow', 'sexy', 'dark', 'bittersweet']
+
+allCategories = [(LOVE, 'LOVE'), (PRETTY, 'PRETTY'), (RELAXING, 'RELAXING'),
+                 (SAD, 'SAD'), (HAPPY, 'HAPPY'), (DREAMY, 'DREAMY'), 
+                 (NOSTALGIC, 'NOSTALGIC'), (MOODY, 'MOODY'), (ANGRY, 'ANGRY'),
+                 (BREAKUP, 'BREAKUP')]
+
+def getAllTags():
+    allTags = []
+    for catTuple in allCategories:
+        for tag in catTuple[0]:
+            allTags.append(tag)
+    return allTags
+
+def getDictAllTags():
+    allTags = {}
+    for catTuple in allCategories:
+        #print catTuple
+        catLabel = catTuple[1]
+        allTags[catLabel] = []
+        for tag in catTuple[0]:
+            allTags[catLabel].append(tag)
+    return allTags
+                
+
+def getTagCategory(tag):
+    # want just category name as string
+    # there is probs a more efficient way f doing this?
+    for catTuple in allCategories:
+        if tag in catTuple[0]:
+            return catTuple[1]
+        
+########## tests ########
+
+# getAllTags()
+# buildDict()
+# getTagCategory('happy')
+
+
+# In[ ]:
+
+
+
---
+++ trim2.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import sqlite3
+import ChosenTags as CT
+import collections
+
+allTags = CT.getAllTags()
+
+def sanitize(tag):
+    """
+    sanitize a tag so it can be included or queried in the db
+
+    author: Thierry Bertin-Mahieux (2011) Columbia University tb2332@columbia.edu
+    """
+    tag = tag.replace("'","''")
+    return tag
+
+def getSongsWithTag(tag, connection):
+    sql = "SELECT tids.tid FROM tags, tid_tag, tids WHERE tags.ROWID = tid_tag.tag AND tids.ROWID = tid_tag.tid and tags.tag = '%s'" % sanitize(tag)
+    res = connection.execute(sql)
+    tracks = res.fetchall()
+    for track in tracks:
+        yield track[0]
+
+def getTagValue(track_id, tag, connection):
+    sql = "SELECT tid_tag.val FROM tid_tag, tids, tags WHERE tags.ROWID = tid_tag.tag AND tid_tag.tid = tids.ROWID AND tids.tid = '%(tid)s' AND tags.tag = '%(tag)s'" % {"tid": track_id, "tag": sanitize(tag)} 
+    res = connection.execute(sql)
+    val = res.fetchone()
+    if val is None:
+        return 0.0
+    else:
+        return val[0]
+
+
+def main():
+    allSongs = {}
+    
+    dbfile = "lastfm_tags.db"
+    conn = sqlite3.connect(dbfile)
+
+    for tag in CT.getAllTags():
+        for song in getSongsWithTag(tag, conn):
+            val = getTagValue(song, tag, conn)
+            category = CT.getTagCategory(tag)            
+            if song in allSongs:
+                allSongs[song][category] += val
+            else:
+                allSongs[song] = collections.defaultdict(float)
+                allSongs[song][category] = val
+    count = 0
+    for k,v in allSongs.iteritems():
+        count += 1
+        print k, ": ", v
+        if count > 135:
+            break
+        
+    conn.close()
+
+
+main()
